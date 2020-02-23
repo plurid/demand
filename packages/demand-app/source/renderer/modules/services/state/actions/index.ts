9078bf6e6567cbd91323362b2af22e2271cc5b01
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-23 18:44:47 +0200

    demand-app > dev: renderer > state, utilities

---
+++ packages/demand-app/source/renderer/modules/services/state/actions/index.ts
@@ -0,0 +1,13 @@
+import * as data from '../modules/data';
+import * as shortcuts from '../modules/shortcuts';
+import * as themes from '../modules/themes';
+import * as ui from '../modules/ui';
+
+
+
+export default {
+    data: data.actions,
+    shortcuts: shortcuts.actions,
+    themes: themes.actions,
+    ui: ui.actions,
+};
---
+++ packages/demand-app/source/renderer/modules/services/state/store/reducers.ts
@@ -0,0 +1,18 @@
+import { combineReducers } from 'redux';
+
+import * as data from '../modules/data';
+import * as shortcuts from '../modules/shortcuts';
+import * as themes from '../modules/themes';
+import * as ui from '../modules/ui';
+
+
+
+const rootReducer = combineReducers({
+    data: data.reducer,
+    shortcuts: shortcuts.reducer,
+    themes: themes.reducer,
+    ui: ui.reducer,
+});
+
+
+export default rootReducer;
