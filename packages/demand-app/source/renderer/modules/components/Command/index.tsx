a200b02bf483aecc81d39ae1714af932dd24d57c
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-23 17:22:47 +0200

    demand-app > dev: renderer > Command, Terminal

--- packages/demand-app/source/renderer/modules/components/Command/index.tsx
+++ packages/demand-app/source/renderer/modules/components/Command/index.tsx
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, {
+    useState,
+} from 'react';
 
 import {
     StyledCommand,
+    StyledCommandMark,
 } from './styled';
+
+import {
+    PluridTextline,
+} from '@plurid/plurid-ui-react';
 
 
 
@@ -13,11 +20,34 @@ const Command: React.FC<CommandProperties> = (
     properties,
 ) => {
     /** properties */
+    // const {
+    // } = properties;
+
+
+    /** state */
+    const [commandValue, setCommandValue] = useState('');
+
+
+    /** handlers */
+    const handleCommandChange = (
+        event: React.ChangeEvent<HTMLInputElement>,
+    ) => {
+        setCommandValue(event.target.value);
+    }
+
 
     /** render */
     return (
         <StyledCommand>
-            >
+            <StyledCommandMark>
+                >
+            </StyledCommandMark>
+
+            <PluridTextline
+                text={commandValue}
+                atChange={handleCommandChange}
+                devisible={true}
+            />
         </StyledCommand>
     );
 }
