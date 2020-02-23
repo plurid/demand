8459e2b58f52b522415cbbe723bb37c29c4a9ba2
Jan Cavel <caveljan@gmail.com>
2020-02-23 16:56:42 +0200

    demand-app > dev: renderer > Page, Pluriverse

--- packages/demand-app/source/renderer/modules/components/Page/index.tsx
+++ packages/demand-app/source/renderer/modules/components/Page/index.tsx
@@ -1,97 +1,24 @@
-import React, {
-    useRef,
-    useContext,
-    useState,
-    useEffect,
-} from 'react';
+import React from 'react';
 
 import {
     StyledPage,
 } from './styled';
 
-import PageBar from './components/PageBar';
-
-import PluriverseContext from '../../containers/Pluriverse/context';
 
 
 
 interface PageProperties {
-    id: string;
 }
 
-const Page: React.FC<PageProperties> = (properties) => {
-    const webviewElement = useRef<HTMLWebViewElement>(null);
+const Page: React.FC<PageProperties> = (
+    properties,
+) => {
+    /** properties */
 
-    const context: any = useContext(PluriverseContext);
-
-    const {
-        pages,
-        updateURL,
-    } = context;
-
-    const {
-        id,
-    } = properties;
-
-    const page = pages.find((page: any) => page.id === id);
-    if (!page) {
-        return (
-            <div>
-                <h4>
-                    Nothing Found.
-                </h4>
-            </div>
-        );
-    }
-
-    const src = page.path;
-
-    const [url, setURL] = useState(src);
-
-    const handleURL = (
-        event: React.ChangeEvent<HTMLInputElement>,
-    ) => {
-        setURL(event.target.value)
-    }
-
-    const handleURLChange = (
-        event: React.KeyboardEvent<HTMLInputElement>,
-    ) => {
-        if (event.key === 'Enter') {
-            const urlRE = /^https?:\/\//;
-            if (urlRE.test(url)) {
-                updateURL(url, id);
-                return;
-            }
-
-            const httpsURL = 'https://' + url;
-            updateURL(httpsURL, id);
-        }
-    }
-
-    useEffect(() => {
-        if (webviewElement.current) {
-            webviewElement.current.addEventListener('dom-ready', async () => {
-                const url = (webviewElement as any).current.getURL();
-                setURL(url);
-            });
-        }
-    }, [
-        webviewElement.current,
-    ]);
-
+    /** render */
     return (
         <StyledPage>
-            <PageBar
-                url={url}
-                handleURL={handleURL}
-                handleURLChange={handleURLChange}
-            />
-
-            <webview
-                src={src}
-                ref={webviewElement}
-            />
+            foo
         </StyledPage>
     );
 }
--- packages/demand-app/source/renderer/modules/containers/Pluriverse/context.ts
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-
-
-
-const PluriverseContext = React.createContext({});
-
-
-export default PluriverseContext;
