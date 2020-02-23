b5480525210fa73438291b6471281c62cbf39621
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2020-02-23 18:05:31 +0200

    demand-app > dev: renderer > logic > command

--- packages/demand-app/source/renderer/modules/services/logic/command.ts
+++ packages/demand-app/source/renderer/modules/services/logic/command.ts
@@ -1,9 +1,12 @@
-const homedir = require('os').homedir();
-
 import {
     execSync,
 } from 'child_process';
 
+import os from 'os';
+
+
+
+const homedir = os.homedir();
 
 
 export const runCommand = (
