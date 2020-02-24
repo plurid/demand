97fbe3af8be92bcdb1d693e5e35a62c16d6364ef
Jan Cavel <caveljan@gmail.com>
2020-02-24 10:36:20 +0200

    demand-app > dev: App, devtools

--- packages/demand-app/source/renderer/modules/services/utilities/devtools.ts
+++ packages/demand-app/source/renderer/modules/services/utilities/devtools.ts
@@ -1,5 +1,11 @@
-import installExtension, { REACT_DEVELOPER_TOOLS } from 'electron-devtools-installer';
+import installExtension, {
+    REDUX_DEVTOOLS,
+} from 'electron-devtools-installer';
 
-installExtension(REACT_DEVELOPER_TOOLS)
-    .then((name) => console.log(`Added Extension:  ${name}`))
-    .catch((err) => console.log('An error occurred: ', err));
+import environment from './environment';
+
+
+
+if (!environment.production) {
+    installExtension(REDUX_DEVTOOLS);
+}
